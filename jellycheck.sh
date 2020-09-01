d4a7354ed031675cd2983e8c28e3c7d0e799e79a
Josh Bowling <josh@balena.io>
2020-09-01 13:21:03 +0900

    Consolidate scripts into one file
    Change-type: patch
    Signed-off-by: Josh Bowling <josh@balena.io>

--- jellycheck.sh
+++ jellycheck.sh
@@ -8,6 +8,65 @@
 
 set -eu
 
+# Check for files in lib/ that shouldn't be there
+if [ -d "lib" ]; then
+	HTML="$(find lib -name '*.html')"
+
+	if [ -n "$HTML" ]; then
+		echo "There can not be HTML files in lib/" 1>&2
+		echo "All deployable components should live in apps" 1>&2
+		echo "" 1>&2
+		echo "$HTML" 1>&2
+		exit 1
+	fi
+
+	WEBPACK="$(find lib -name '*webpack*.js*')"
+
+	if [ -n "$WEBPACK" ]; then
+		echo "There can not be webpack configuration files in lib/" 1>&2
+		echo "All deployable components should live in apps" 1>&2
+		echo "" 1>&2
+		echo "$WEBPACK" 1>&2
+		exit 1
+	fi
+
+	DOCKERFILE="$(find lib -name '*Dockerfile*')"
+
+	if [ -n "$DOCKERFILE" ]; then
+		echo "There can not be Dockefiles in lib/" 1>&2
+		echo "All deployable components should live in apps" 1>&2
+		echo "" 1>&2
+		echo "$DOCKERFILE" 1>&2
+		exit 1
+	fi
+fi
+
+# Check filenames
+DIRECTORIES=("lib" "scripts")
+for directory in "${DIRECTORIES}"; do
+	if [ -d "$directory" ]; then
+		for file in $(find "$directory" -type f | grep -v -E node_modules); do
+			BASENAME="$(basename "$file")"
+
+			# Known exceptions
+			if [ "$BASENAME" = "LICENSE" ] || [ "$BASENAME" = "README.md" ]; then
+				continue
+			fi
+
+			# Everything that is all lowercase is fine
+			if ! [[ $file =~ [A-Z] ]]; then
+				continue
+			fi
+
+			echo "This file should not have capital letters:" 1>&2
+			echo "" 1>&2
+			echo "$file" 1>&2
+			exit 1
+		done
+	fi
+done
+
+# Check license header
 LICENSE_JS="/*
  * Copyright (C) Balena.io - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited.
